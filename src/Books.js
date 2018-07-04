23771f9e69375bbd02fb01683b807d661c5ba76b
Vally <geanny_vally@yahoo.com>
2018-07-04 13:58:24 +0300

    ShelfTemplate and changing shelves not working

--- src/Books.js
+++ src/Books.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import PropTypes from 'prop-types'
+import Shelf from './ShelfTemplate'
 
 class Books extends Component {
   state = {
@@ -9,21 +10,88 @@ class Books extends Component {
     wantToRead : [],
     read : []
   }
+
   static propTypes={
     ourBooks: PropTypes.array.isRequired,
   }
 
-  componentDidMount(){
+  removeBook(book){
+        let arrayBook=[];
+        let index=-1;
+
+        if (book.shelf === "currentlyReading")  {
+            index=this.props.currentlyReading.indexOf(book);
+            arrayBook=this.props.currentlyReading;
+        } else if( book.shelf === "wantToRead" ){
+            index=this.props.wantToRead.indexOf(book);
+            arrayBook=this.props.wantToRead;
+        } else {
+            index=this.props.read.indexOf(book);
+            arrayBook=this.props.read;
+        }
+        arrayBook.splice(index,1);
+        return arrayBook;
+    }
+
+    updateShelf(fromShelf,newArray){
+        if (fromShelf === "currentlyReading")  {
+         this.setState({ currentlyReading: newArray})
+       } else if( fromShelf === "wantToRead" ){
+            this.setState({ wantToRead: newArray})
+        } else {
+            this.setState({ read: newArray})
+        }
+    }
+
+    updateToShelf(toShelf,toNewArray){
+        if (toShelf === "currentlyReading")  {
+         this.setState({ currentlyReading: toNewArray})
+       } else if( toShelf === "wantToRead" ){
+            this.setState({ wantToRead: toNewArray})
+        } else {
+            this.setState({ read: toNewArray})
+        }
+    }
+
+    onChangeShelf(book,event){
+        let tmpBook=book;
+        let fromShelf=book.shelf;
+        let toShelf=event.target.value;
+        let toNewArray=[];
+
+        if (event.target.value!=="none" && book.shelf !==event.target.value){
+            let newArray=this.removeBook(book);
+
+            if (toShelf === "currentlyReading")  {
+                   this.updateShelf(fromShelf,newArray);
+                   toNewArray=this.props.currentlyReading;
+                   book.shelf=toShelf;
+                   toNewArray.push(book);
+                   this.updateToShelf(toShelf,toNewArray);
+            }else if( toShelf === "wantToRead" ){
+                this.updateShelf(fromShelf,newArray);
+                toNewArray=this.props.wantToRead;
+                book.shelf=toShelf;
+                toNewArray.push(book);
+                this.updateToShelf(toShelf,toNewArray);
+            }else {
+                this.updateShelf(fromShelf,newArray);
+                toNewArray=this.props.read;
+                book.shelf=toShelf;
+                toNewArray.push(book);
+                this.updateToShelf(toShelf,toNewArray);
+            }
+        } else if (event.target.value ==="none" && book.shelf !==event.target.value){
+            let newArray=this.removeBook(book);
+            book.shelf="none";
+            this.updateShelf(fromShelf,newArray);
+        }
+    }
+
+  render(){
     let currentlyReading=this.props.ourBooks.filter(book=>book.shelf==="currentlyReading");
     let wantToRead=this.props.ourBooks.filter(book=>book.shelf==="wantToRead");
     let read=this.props.ourBooks.filter(book=>book.shelf==="read");
-    this.setState({ currentlyReading: currentlyReading,wantToRead: wantToRead,read: read });
-
-    console.log(this.props)
-
-  }
-
-  render(){
     return(
       <div className="list-books">
         <div className="list-books-title">
@@ -31,87 +99,9 @@ class Books extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                {this.state.currentlyReading.map((book)=>(
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
-                        <div className="book-shelf-changer">
-                          <select id="selector">
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                ))}
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                {this.state.wantToRead.map((book)=>(
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
-                        <div className="book-shelf-changer">
-                          <select id="selector">
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                ))}
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                {this.state.read.map((book)=>(
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                ))}
-                </ol>
-              </div>
-            </div>
+            <Shelf title='Currently Reading' currentlyReading={this.state.currentlyReading} wantToRead={this.state.wantToRead} read={this.state.read} shelf={currentlyReading=this.props.ourBooks.filter(book=>book.shelf==="currentlyReading")}/>
+            <Shelf title='Want to Read' currentlyReading={this.state.wantToRead} wantToRead={this.state.wantToRead} read={this.state.read} shelf={wantToRead=this.props.ourBooks.filter(book=>book.shelf==="wantToRead")}/>
+            <Shelf title='read' currentlyReading={this.state.read} wantToRead={this.state.wantToRead} read={this.state.read} shelf={read=this.props.ourBooks.filter(book=>book.shelf==="read")}/>
           </div>
         </div>
         <div className="open-search">
---
+++ src/ShelfTemplate.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Books  from './Books'
+
+class Shelf extends Component {
+
+  render(){
+    return(
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          {this.props.shelf.map((book)=>(
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${book.imageLinks.thumbnail}')` }}></div>
+                  <div className="book-shelf-changer">
+                    <select id="selector" value={book.shelf} onChange={(event)=>this.props.onChangeShelf()}>
+                      <option value="move" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
