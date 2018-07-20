8549947f3bf82ca793b81743157e39884036f9eb
Vally <geanny_vally@yahoo.com>
2018-07-20 12:36:27 +0300

    Change updateShelf() & update Books.js for Book template

--- src/App.js
+++ src/App.js
@@ -7,117 +7,32 @@ import './App.css'
 
 class BooksApp extends Component {
   state = {
-    books: [],
-    ourBooks: [],
-    currentlyReading : [],
-    wantToRead : [],
-    read : []
+    books: []
+  }
+
+  updateShelf = (book, shelf) => {
+    let books;
+    if (this.state.books.findIndex(b => b.id === book.id) > 0) {
+      books = this.state.books.map(b => {
+        if (b.id === book.id) {
+          return {...book, shelf}
+        } else {
+          return b
+        }
+      })
+    } else {
+      //new book to the shelf
+      books = [...this.state.books, {...book, shelf}]
+    }
+    this.setState({books})
+    BooksAPI.update(book, shelf).then((data) => {})
   }
 
   componentDidMount(){
-    BooksAPI.getAll().then((ourBooks) => {
-      this.setState({ ourBooks })
-    }),
-    BooksAPI.search().then((books)=>{
+    BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
   }
-
-  removeBook = (book) => {
-        let arrayBook=[];
-        let index=-1;
-
-        if (book.shelf === "currentlyReading")  {
-            index=this.state.currentlyReading.indexOf(book);
-            arrayBook=this.state.currentlyReading;
-        } else if( book.shelf === "wantToRead" ){
-            index=this.state.wantToRead.indexOf(book);
-            arrayBook=this.state.wantToRead;
-        } else {
-            index=this.state.read.indexOf(book);
-            arrayBook=this.state.read;
-        }
-        arrayBook.splice(index,1);
-        return arrayBook;
-    }
-
-    updateShelf = (fromShelf,newArray) =>{
-        if (fromShelf === "currentlyReading")  {
-         this.setState({ currentlyReading: newArray})
-       } else if( fromShelf === "wantToRead" ){
-            this.setState({ wantToRead: newArray})
-        } else {
-            this.setState({ read: newArray})
-        }
-    }
-
-    updateToShelf = (toShelf,toNewArray) => {
-        if (toShelf === "currentlyReading")  {
-         this.setState({ currentlyReading: toNewArray})
-       } else if( toShelf === "wantToRead" ){
-            this.setState({ wantToRead: toNewArray})
-        } else {
-            this.setState({ read: toNewArray})
-        }
-    }
-
-    onChangeShelf = (book,event) => {
-        let tmpBook=book;
-        let fromShelf=book.shelf;
-        let toShelf=event.target.value;
-        let toNewArray=[];
-
-        if (event.target.value!=="none" && book.shelf !==event.target.value){
-            let newArray=this.removeBook(book);
-
-            if (toShelf === "currentlyReading")  {
-                   this.updateShelf(fromShelf,newArray);
-                   toNewArray=this.state.currentlyReading;
-                   book.shelf=toShelf;
-                   toNewArray.push(book);
-                   this.updateToShelf(toShelf,toNewArray);
-            }else if( toShelf === "wantToRead" ){
-                this.updateShelf(fromShelf,newArray);
-                toNewArray=this.state.wantToRead;
-                book.shelf=toShelf;
-                toNewArray.push(book);
-                this.updateToShelf(toShelf,toNewArray);
-            }else {
-                this.updateShelf(fromShelf,newArray);
-                toNewArray=this.state.read;
-                book.shelf=toShelf;
-                toNewArray.push(book);
-                this.updateToShelf(toShelf,toNewArray);
-            }
-        } else if (event.target.value ==="none" && book.shelf !==event.target.value){
-            let newArray=this.removeBook(book);
-            book.shelf="none";
-            this.updateShelf(fromShelf,newArray);
-        } else if (event.target.value !== book.shelf && book.shelf ==="none"){
-          let newArray=this.removeBook(book);
-
-          if (toShelf === "currentlyReading")  {
-                 this.updateShelf(fromShelf,newArray);
-                 toNewArray=this.state.currentlyReading;
-                 book.shelf=toShelf;
-                 toNewArray.push(book);
-                 this.updateToShelf(toShelf,toNewArray);
-          }else if( toShelf === "wantToRead" ){
-              this.updateShelf(fromShelf,newArray);
-              toNewArray=this.state.wantToRead;
-              book.shelf=toShelf;
-              toNewArray.push(book);
-              this.updateToShelf(toShelf,toNewArray);
-          }else {
-              this.updateShelf(fromShelf,newArray);
-              toNewArray=this.state.read;
-              book.shelf=toShelf;
-              toNewArray.push(book);
-              this.updateToShelf(toShelf,toNewArray);
-          }
-        }
-    }
-
 
   render() {
     return (
@@ -125,13 +40,13 @@ class BooksApp extends Component {
         <Route path='/search' render={() => (
           <Search
             books={this.state.books}
-            onChangeShelf={this.onChangeShelf}
+            onChangeShelf={(book, shelf) => this.updateShelf(book, shelf)}
            />
         )}/>
         <Route exact path='/' render={({ history }) => (
           <Books
-            ourBooks={this.state.ourBooks}
-            onChangeShelf={this.onChangeShelf}
+            books={this.state.books}
+            onChangeShelf={(book, shelf) => this.updateShelf(book, shelf)}
             />
         )}/>
       </div>
--- src/Books.js
+++ src/Books.js
@@ -1,37 +1,61 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import PropTypes from 'prop-types'
-import Shelf from './ShelfTemplate'
+import Book from './SearchedBook'
 
 class Books extends Component {
-  state = {
-    currentlyReading : [],
-    wantToRead : [],
-    read : []
+  updateBook = (book, shelf) => {
+    this.props.onChangeShelf(book, shelf)
   }
 
   render(){
-    let currentlyReading=this.props.ourBooks.filter(book=>book.shelf==="currentlyReading");
-    let wantToRead=this.props.ourBooks.filter(book=>book.shelf==="wantToRead");
-    let read=this.props.ourBooks.filter(book=>book.shelf==="read");
     return(
       <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Shelf title='Currently Reading' onChangeShelf={this.props.onChangeShelf} shelf={currentlyReading=this.props.ourBooks.filter(book=>book.shelf==="currentlyReading")}/>
-            <Shelf title='Want to Read' onChangeShelf={this.props.onChangeShelf} shelf={wantToRead=this.props.ourBooks.filter(book=>book.shelf==="wantToRead")}/>
-            <Shelf title='read' onChangeShelf={this.props.onChangeShelf} shelf={read=this.props.ourBooks.filter(book=>book.shelf==="read")}/>
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    { this.props.books.filter(book => book.shelf === 'currentlyReading')
+                      .map(book => (
+                        <Book key={book.id} book={book} onChangeShelf={(book, shelf) => this.updateBook(book, shelf)}/>
+                      ))}
+                  </ol>
+                </div>
+              </div>
+
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    { this.props.books.filter(book => book.shelf === 'wantToRead')
+                      .map(book => (
+                        <Book key={book.id} book={book} onChangeShelf={(book, shelf) => this.updateBook(book, shelf)}/>
+                      ))}
+                  </ol>
+                </div>
+              </div>
+
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    { this.props.books.filter(book => book.shelf === 'read')
+                      .map(book => (
+                        <Book key={book.id} book={book} onChangeShelf={(book, shelf) => this.updateBook(book, shelf)}/>
+                      ))}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="open-search">
+            <Link to="/search">Add a book</Link>
           </div>
         </div>
-        <div className="open-search">
-          <Link to='/search'>Add a book</Link>
-        </div>
-      </div>
-
     )
   }
 }
